61735a00e59c4218771b503e2c3f5ab2a54e652a
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2024-05-15 11:51:58 +0200

    Add movement function

--- control.py
+++ control.py
@@ -1,3 +1,20 @@
 from martypy import Marty
-my_marty = Marty("wifi", "192.168.0.100")
 
+def toward(marty):
+    marty.walk()
+
+def backward(marty):
+    marty.walk(40,"auto",4.5)
+    marty.walk(10)
+
+def left(marty):
+    marty.stand_straight()
+    marty.walk(20,"auto",4,5)
+
+def right(marty):
+    marty.stand_straight()
+    marty.walk(20,"auto",-4.5)
+
+my_marty = Marty("wifi","192.168.0.2")
+toward(my_marty)
+backward(my_marty)
