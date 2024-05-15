fd0357a5998be40a84b7249ca45982aab2ddaaca
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2024-05-15 10:43:54 +0200

    Add emotion control

---
+++ emotion.py
@@ -0,0 +1,13 @@
+from martypy import Marty
+import time
+
+def angry(marty):
+    marty.eyes("angry",500)
+    time.sleep(1)
+    marty.eyes("normal",1000)
+
+def excited(marty):
+    marty.eyes("excited",500)
+    time.sleep(1)
+    marty.eyes("normal",1000)
+
