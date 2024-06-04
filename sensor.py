6a4682a3df09e3ac401a6b8fbda838259f010ca7
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2024-06-04 15:17:36 +0200

    fix: color recognition

--- sensor.py
+++ sensor.py
@@ -56,7 +56,7 @@ monitor_battery()
 #     print(nme + " = " + str(val));
 
 
-# my_marty.walk(30, 'auto', 0 , -25, 1500, None);
+# my_marty.walk(20, 'auto', 0 , -25, 1500, None);
 
 # # Foot sensor 
 # leftSensor = my_marty.get_ground_sensor_reading("left");
@@ -83,58 +83,70 @@ monitor_battery()
 # #     print(value);
 
 def calibrate_yellow(marty):
-    tableau = []
-    for _ in range(50):
-        valeur = marty.get_color_sensor_hex("left")
-        tableau.append(valeur)
-        time.sleep(0.02)
-    return tableau
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
     
 def calibrate_green(marty):
-    tableau = []
-    for _ in range(20):
-        valeur = marty.get_color_sensor_hex("left")
-        tableau.append(valeur)
-        time.sleep(0.1)
-    return tableau
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
 
 def calibrate_purple(marty):
-    tableau = []
-    for _ in range(20):
-        valeur = marty.get_color_sensor_hex("left")
-        tableau.append(valeur)
-        time.sleep(0.1)
-    return tableau
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
 
 def calibrate_red(marty):
-    tableau = []
-    for _ in range(20):
-        valeur = marty.get_color_sensor_hex("left")
-        tableau.append(valeur)
-        time.sleep(0.1)
-        
-    return tableau
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
+
+def calibrate_cyan(marty):
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
 
 def calibrate_blue(marty):
-    tableau = []
-    for _ in range(20):
-        valeur = marty.get_color_sensor_hex("left")
-        tableau.append(valeur)
-        time.sleep(0.1)
-    return tableau
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
 
-def color_sensor(yellow, green, purple, red, blue, marty):
-    color = marty.get_color_sensor_hex("left");
-    if color in yellow:
+def calibrate_black(marty):
+    valeurHex = marty.get_color_sensor_hex("left");
+    valeurRGB = hex_to_rgb(valeurHex);
+    return valeurRGB;
+
+def color_sensor(yellow, green, purple, red, blue, cyan, black, marty):
+    colorHEX = marty.get_color_sensor_hex("left");
+    colorRGB = hex_to_rgb(colorHEX);
+    if ((is_within_uncertainty(colorRGB[0], yellow[0], 10)) and (is_within_uncertainty(colorRGB[1], yellow[1], 10)) and (is_within_uncertainty(colorRGB[2], yellow[2], 10))):
         return "yellow";
-    elif color in green:
+    elif ((is_within_uncertainty(colorRGB[0], green[0], 10)) and (is_within_uncertainty(colorRGB[1], green[1], 10)) and (is_within_uncertainty(colorRGB[2], green[2], 10))):
         return "green";
-    elif color in purple:
+    elif ((is_within_uncertainty(colorRGB[0], purple[0], 10)) and (is_within_uncertainty(colorRGB[1], purple[1], 10)) and (is_within_uncertainty(colorRGB[2], purple[2], 10))):
         return "purple";
-    elif color in red:
+    elif ((is_within_uncertainty(colorRGB[0], red[0], 10)) and (is_within_uncertainty(colorRGB[1], red[1], 10)) and (is_within_uncertainty(colorRGB[2], red[2], 10))):
         return "red";
-    elif color in blue:
+    elif ((is_within_uncertainty(colorRGB[0], blue[0], 10)) and (is_within_uncertainty(colorRGB[1], blue[1], 10)) and (is_within_uncertainty(colorRGB[2], blue[2], 10))):
         return "blue";
+    elif ((is_within_uncertainty(colorRGB[0], cyan[0], 10)) and (is_within_uncertainty(colorRGB[1], cyan[1], 10)) and (is_within_uncertainty(colorRGB[2], cyan[2], 10))):
+        return "cyan";
+    elif ((is_within_uncertainty(colorRGB[0], black[0], 10)) and (is_within_uncertainty(colorRGB[1], black[1], 10)) and (is_within_uncertainty(colorRGB[2], black[2], 10))):
+        return "black";
     else:
         return "not defined";
+
+
+
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    return rgb_color
+
+def is_within_uncertainty(value, central_value, uncertainty):
+    lower_bound = central_value - uncertainty
+    upper_bound = central_value + uncertainty
+    return lower_bound <= value <= upper_bound
+
 
